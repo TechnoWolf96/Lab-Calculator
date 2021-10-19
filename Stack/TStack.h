1e51c1b2aa66cada9d2dae885838a0250bf170cf
TechnoWolf96 <v.alentin52.v@yandex.ru>
2021-10-19 17:15:46 +0300

    Released TStack

---
+++ Stack/TStack.h
@@ -0,0 +1,109 @@
+#pragma once
+
+
+
+// Класс "Стек" со структурой хранения элементов в массиве
+template<class T>
+class TStack
+{
+private:
+	T* arr;			// Массив, в котором хранятся элементы стека
+	int size;		// Размер стека
+	int pos;		// Позиция последнего элемента в стеке
+public:
+
+	TStack(int _size = 10);						// Конструктор
+	TStack(const TStack& other);				// Конструктор копирования
+	TStack<T>& operator=(const TStack<T>& other);	// Оператор присваивания
+	~TStack() { delete[] arr; }					// Деструктор
+
+	int Count() { return pos + 1; }				// Количество элементов в стеке
+	bool Empty();								// Пустой ли стек
+	bool Full();								// Заполнен ли стек
+	void Push(T item);							// Добавить элемент в стек
+	T Pop();									// Извлечь последний элемент из стека
+	T Top() { return arr[pos]; }				// Посмотреть последний элемент в стеке
+	void Clear() { pos = -1; }					// Очистить стек
+
+	bool operator!=(const TStack<T>& other);		// Операторы сравнения
+	bool operator==(const TStack<T>& other);
+	
+};
+
+template<class T>
+inline TStack<T>::TStack(int _size)
+{
+	if (_size <= 0) throw "Wrong size";
+	size = _size;
+	arr = new T[size];
+	pos = -1;
+}
+
+template<class T>
+inline TStack<T>::TStack(const TStack& other)
+{
+	size = other.size;
+	arr = new T[size];
+	pos = other.pos;
+	for (int i = 0; i <= pos; i++)
+		arr[i] = other.arr[i];
+}
+
+template<class T>
+inline bool TStack<T>::Empty()
+{
+	if (pos == -1) return true;
+	else return false;
+}
+
+template<class T>
+inline bool TStack<T>::Full()
+{
+	return pos >= size-1;
+}
+
+template<class T>
+inline void TStack<T>::Push(T item)
+{
+	if (Full()) throw "Stack owerflow";
+	arr[++pos] = item;
+}
+
+template<class T>
+inline T TStack<T>::Pop()
+{
+	if (Empty()) throw "Stack is empty";
+	T temp = arr[pos];
+	pos--;
+	return temp;
+}
+
+template<class T>
+inline TStack<T>& TStack<T>::operator=(const TStack<T>& other)
+{
+	if (size != other.size)
+	{
+		delete[] arr;
+		size = other.size;
+		arr = new T[size];
+	}
+	pos = other.pos;
+	for (int i = 0; i <= pos; i++)
+		arr[i] = other.arr[i];
+	return *this;
+}
+
+template<class T>
+inline bool TStack<T>::operator!=(const TStack& other)
+{
+	return !operator==(other);
+}
+
+template<class T>
+inline bool TStack<T>::operator==(const TStack& other)
+{
+	if (pos != other.pos) return false;
+	for (int i = 0; i < pos; i++)
+		if (arr[i] != other.arr[i]) return false;
+	return true;
+}
---
+++ Stack/Test.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include "TStack.h"
+using namespace std;
+
+void Test()
+{
+    int size = 5;
+    int items[5] = { 1, 5, 2, 8, -12 };
+
+    cout << "\n===== Тестирование класса TStack =====" << endl;
+
+    TStack<int> a(size);
+    for (int i = 0; i < size; i++) a.Push(items[i]);
+    cout << "\nСтек А заполнен. a.Full() = " << a.Full() << ", a.Empty() = " << a.Empty() << ", a.Count() = " << a.Count();
+
+    TStack<int> b(a);
+    cout << "\nВ стек В скопирован стек А. b.Full() = " << b.Full() << ", b.Empty() = " << b.Empty() << ", b.Count() = " << b.Count();
+    cout << "\nНа вершине А лежит: " << a.Top() << "; На вершине B лежит: " << b.Top();
+    int q = b.Pop();
+    cout << "\nИз В извлечен элемент " << q << "; На вершине B лежит: " << b.Top() << "; b.Full() = " << b.Full() << ", b.Empty() = " << b.Empty() << ", b.Count() = " << b.Count();
+    cout << "\nСравнение: A!=B: " << (a != b) << "; A == B: " << (a == b);
+
+    cout << "\nВ стеке А находились элементы: ";
+    while (!a.Empty()) cout << a.Pop() << ' ';
+    cout << "\nВ стеке В находились элементы: ";
+    while (!b.Empty()) cout << b.Pop() << ' ';
+    cout << "\nСтек А опустел. Full = " << a.Full() << ", Empty = " << a.Empty() << ", a.Count() = " << a.Count();
+    cout << "\nСтек B опустел. Full = " << b.Full() << ", Empty = " << b.Empty() << ", a.Count() = " << b.Count();
+
+    for (int i = 0; i < size; i++) a.Push(items[i]);
+    cout << "\nСтек А заполнен. a.Full() = " << a.Full() << ", a.Empty() = " << a.Empty() << ", a.Count() = " << a.Count();
+    a.Clear();
+    cout << "\nСтек А: Применен Clear(). a.Full() = " << a.Full() << ", a.Empty() = " << a.Empty() << ", a.Count() = " << a.Count();
+
+}
+
+bool BracketsTask(string str) // Задача про скобки, возвращает верно ли расставлены скобки
+{
+    TStack<char> stack(str.size());
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '(') stack.Push('(');
+        try
+        {
+            if (str[i] == ')') stack.Pop();
+        }
+        catch (...)
+        {
+            return false;
+        }
+    }
+    if (!stack.Empty()) return false;
+    return true;
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    string str;
+    getline(cin, str);
+    if (BracketsTask(str)) cout << "\nСкобки расставлены верно.";
+    else cout << "\nСкобки расставлены неверно.";
+    
+    cout << '\n';
+    system("pause");
+}
+
