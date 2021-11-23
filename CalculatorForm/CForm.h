e7b29003a2ece5b05b8a1ce2b81dc09c8be9bd6a
TechnoWolf96 <v.alentin52.v@yandex.ru>
2021-11-23 18:45:28 +0300

    Реализован графический интерфейс

--- CalculatorForm/CForm.h
+++ CalculatorForm/CForm.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "../Stack/TCalculator.h"
+#include <msclr\marshal_cppstd.h>
+#include <string>
 
 namespace CalculatorForm {
 
@@ -34,6 +37,14 @@ namespace CalculatorForm {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +59,140 @@ namespace CalculatorForm {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"CForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(58, 89);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(642, 43);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &CForm::textBox1_KeyPress);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(227, 42);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(292, 32);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Введите выражение:";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(273, 186);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(192, 52);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Вычислить!";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &CForm::button1_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->BackColor = System::Drawing::SystemColors::Window;
+			this->textBox2->Enabled = false;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(217, 335);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(483, 43);
+			this->textBox2->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(52, 338);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(159, 32);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Результат:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(46, 401);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(165, 64);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Инфиксная\r\n   запись:";
+			// 
+			// textBox3
+			// 
+			this->textBox3->BackColor = System::Drawing::SystemColors::Window;
+			this->textBox3->Enabled = false;
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox3->Location = System::Drawing::Point(217, 411);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(483, 43);
+			this->textBox3->TabIndex = 5;
+			// 
+			// CForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(758, 518);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"CForm";
+			this->Text = L"Калькулятор выражений";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+
+
+private: System::Void textBox1_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((Char::IsLetter(e->KeyChar) && e->KeyChar != 'e'))
+		e->Handled = true;
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	string expr;
+	expr = msclr::interop::marshal_as<std::string>(textBox1->Text);
+	try
+	{
+		TCalculator a(expr);
+		textBox2->Text = Convert::ToString(a.Calculate());
+		textBox3->Text = msclr::interop::marshal_as<String^>(a.GetPostfix());
+	}
+	catch (...)
+	{
+		textBox2->Text = "Выражение задано неверно";
+		textBox3->Text = "";
+	}
+	
+}
+
+
+};
 }
